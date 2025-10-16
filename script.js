e3e8780c1fb4cdca68b6cadcbef4583f77c10892
yassine-barboura <barbouramohamedyassine@issatso.u-sousse.tn>
2025-10-16 22:39:21 +0100

    Add Pig Game initial implementation and assets
    Introduces the Pig Game web app with HTML, CSS, and JavaScript files. Adds dice images, game UI, and core logic for rolling, holding, and switching players. Implements win condition and new game functionality.

---
+++ script.js
@@ -0,0 +1,80 @@
+"use strict";
+const score0 = document.querySelector("#score--0");
+const score1 = document.querySelector("#score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
+const dice_element = document.querySelector(".dice");
+const btn_game = document.querySelector(".btn--game");
+const btn_roll = document.querySelector(".btn--roll");
+const btn_hold = document.querySelector(".btn--hold");
+
+let scores, currentScore, activePlayer, playing;
+
+//init
+const init = function () {
+  activePlayer = 0;
+  scores = [0, 0];
+  currentScore = 0;
+  playing = true;
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  dice_element.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player1El.classList.remove("player--active");
+  player0El.classList.add("player--active");
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+
+//rolling the dice
+btn_roll.addEventListener("click", function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    dice_element.classList.remove("hidden");
+    dice_element.src = `/images/dice-${dice}.png`;
+    console.log(dice);
+    console.log(activePlayer);
+    if (dice === 1) {
+      switchPlayer();
+    } else {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
+  }
+});
+
+btn_game.addEventListener("click", init);
+
+btn_hold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 20) {
+      playing = false;
+      dice_element.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
